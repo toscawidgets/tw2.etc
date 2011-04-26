43bd504d6a1bba31a5dc669a5d7d42320d25df5b
Ralph Bean <ralph.bean@gmail.com>
2011-04-26 14:37:03 -0400

    Added synonym for src.

--- tw2/etc/widgets.py
+++ tw2/etc/widgets.py
@@ -6,3 +6,9 @@ class IFrameWidget(twc.Widget):
     height = twc.Param(default='100%', attribute=True)
     width = twc.Param(default='100%', attribute=True)
     src = twc.Param("Source for the iframe", attribute=True)
+    url = twc.Param("Synonym for src", default=None)
+
+    def prepare(self):
+        if self.url:
+            self.src = self.url
+        super(IFrameWidget, self).prepare()
